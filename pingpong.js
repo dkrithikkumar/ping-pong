243d065e19980c442525c2a26185023220564398
Krithik Kumar <dkrithik2322@gmail.com>
2021-09-28 21:55:44 +0530

    Collision Detection and User Paddle Control Done

--- pingpong.js
+++ pingpong.js
@@ -64,6 +64,43 @@ const drawNet = () => {
         drawRectangle(net.x, net.y + i, net.width, net.height, net.color);
 }
 
+canvas.addEventListener("mousemove", (event) => {
+    let rect = canvas.getBoundingClientRect();
+    user.y = event.clientY - rect.top - user.height / 2;
+})
+
+function collisionDetection(ball, player) {
+    ball.top = ball.y - ball.radius;
+    ball.bottom = ball.y + ball.radius;
+    ball.left = ball.x - ball.radius;
+    ball.right = ball.x + ball.radius;
+
+    player.top = player.y;
+    player.bottom = player.y + player.height;
+    player.left = player.x;
+    player.right = player.x + player.width;
+
+    return ball.right > player.left && ball.bottom > player.top && ball.left < player.right && ball.top < player.bottom;
+}
+
+function update() {
+    ball.x += ball.velX;
+    ball.y += ball.velY;
+
+    /* Simple AI to control the computer paddle */
+    let difficultyFactor = 0.1;
+    computer.y += (ball.y - (computer.y + computer.height / 2)) * difficultyFactor;
+
+    if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+        ball.velY = -ball.velY;
+    }
+
+    let whichPlayer = (ball.x < canvas.width / 2) ? user : computer;
+    if (collisionDetection(ball, whichPlayer))
+        ball.velX = -ball.velX;
+
+}
+
 function render() {
     drawRectangle(0, 0, canvas.width, canvas.height, "#000");
     drawNet();
@@ -75,6 +112,7 @@ function render() {
 }
 
 function game() {
+    update();
     render();
 }
 
